431578bed100addadec149f03d0c82c9f8a61b1b
mengdc <3300153701@qq.com>
2023-06-15 00:55:14 +0800

    one

---
+++ src/MyTest.php
@@ -0,0 +1,21 @@
+<?php
+
+class MyTest{
+
+
+    public function __construct()
+    {
+        echo 'MyTest construct';
+    }
+
+    public static function getInstance(){
+        static $_instance = null;
+        if (empty($_instance)){
+            return new static();
+        }
+    }
+
+    public function getTest(){
+        echo 'MyTest getTest';
+    }
+}
